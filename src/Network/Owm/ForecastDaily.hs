ea8823a424812852917a0048a422eb50bf1c3a3f
mayeranalytics <mmayer@mayeranalytics.com>
2018-11-07 10:54:24 +0100

    split away ForcastDaily

---
+++ src/Network/Owm/ForecastDaily.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings   #-}
+
+module Network.Owm.ForecastDaily (
+    module Network.Owm.Internal.ForecastDaily
+) where
+
+import           System.Exit        (exitFailure, exitSuccess)
+import           System.IO          (stderr, hPutStrLn)
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import           System.Environment (getArgs)
+import           Control.Monad      (forM_, mzero, join)
+import           Control.Applicative
+import           Data.Aeson.AutoType.Alternative
+import           Data.Aeson(decode, Value(..), FromJSON(..), ToJSON(..),
+                            pairs,
+                            (.:), (.:?), (.=), object)
+import           Data.Monoid
+import           Data.Text (Text)
+import qualified GHC.Generics
+import           Network.Owm.Internal.ForecastDaily
