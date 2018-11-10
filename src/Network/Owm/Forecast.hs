d1cfca0bf4b67f24640658e5f66e05fafd8710db
mayeranalytics <mmayer@mayeranalytics.com>
2018-11-10 15:48:28 +0100

    added calls

--- src/Network/Owm/Forecast.hs
+++ src/Network/Owm/Forecast.hs
@@ -1,13 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Network.Owm.Forecast (
-    module Network.Owm.Internal.Forecast
+    module Network.Owm.Internal.Forecast,
+    fromCityId, fromCityName, fromCoords, fromZipCode
 ) where
 
-import qualified Data.ByteString.Lazy as BSL
-import           Data.Aeson(decode)
-import           Network.Wreq (get, responseBody)
-import           Control.Lens ((.~), (^.))
-import           Network.Owm.Internal.Forecast hiding(parse)
-import qualified Network.Owm as Owm
-    
+import           Network.Owm.Internal.Forecast (Forecast)
+import           Network.Owm
+
+fromCityId :: Key -> Units -> Lang -> CityId -> Count -> IO (Maybe Forecast)
+fromCityId key units lang id_ count = 
+    getOwmWith [makeParam id_, makeParam count] key units lang (owmBaseUrl25 ++ "/forecast/?")
+
+fromCityName :: Key -> Units -> Lang -> CityName -> Count -> IO (Maybe Forecast)
+fromCityName key units lang name count = 
+    getOwmWith [makeParam name, makeParam count] key units lang (owmBaseUrl25 ++ "/forecast/?") where
+
+fromCoords :: Key -> Units -> Lang -> Coords -> Count -> IO (Maybe Forecast)
+fromCoords key units lang coords count = 
+    getOwmWith [makeParam coords, makeParam count] key units lang (owmBaseUrl25 ++ "/forecast/?")
+
+fromZipCode :: Key -> Units -> Lang -> ZipCode -> Count -> IO (Maybe Forecast)
+fromZipCode key units lang zipcode count = 
+    getOwmWith [makeParam zipcode, makeParam count] key units lang (owmBaseUrl25 ++ "/forecast/?") where
