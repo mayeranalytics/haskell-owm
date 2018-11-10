3d000a64b313cc64e547979ef190dc80bfdf409a
mayeranalytics <mmayer@mayeranalytics.com>
2018-11-10 15:47:39 +0100

    add forecast, forecastdaily examples

--- Main.hs
+++ Main.hs
@@ -5,8 +5,11 @@ import qualified Data.ByteString.Lazy.Char8 as BSL
 import           Data.String.Utils          (strip)
 import           Network.HTTP
 import qualified Network.Owm                as Owm
-import qualified Network.Owm.Forecast       as Owm.Forecast
-import qualified Network.Owm.Weather        as Owm.Weather
+import           Network.Owm                (CityName(..), CityId(..), Coords(..), CountryCode,
+                                             ZipCode(..), Count(..))
+import qualified Network.Owm.Forecast       as Forecast
+import qualified Network.Owm.ForecastDaily  as ForecastDaily
+import qualified Network.Owm.Weather        as Weather
 
 getKey :: FilePath -> IO Owm.Key
 getKey f = (Owm.Key . strip) <$> readFile f
@@ -14,7 +17,16 @@ getKey f = (Owm.Key . strip) <$> readFile f
 main :: IO()
 main = do
     key <- getKey "owm.key"
-    print =<< Owm.Weather.fromCityName key Owm.Metric Owm.EN "Altendorf" (Just "CH") 
-    print =<< Owm.Weather.fromCityId key Owm.Metric Owm.EN 2172797
-    print =<< Owm.Weather.fromCoords key Owm.Metric Owm.EN (12,47)
-    print =<< Owm.Weather.fromZipCode key Owm.Metric Owm.EN 8001 (Just "CH") 
+    {-
+    -- Weather
+    print =<< Weather.fromCityName key Owm.Metric Owm.EN (CityName "Altendorf" "CH") 
+    print =<< Weather.fromCityId key Owm.Metric Owm.EN (CityId 2172797)
+    print =<< Weather.fromCoords key Owm.Metric Owm.EN (Owm.Coords 47.37643 8.54782)
+    print =<< Weather.fromZipCode key Owm.Metric Owm.EN (ZipCode 8001 "CH")
+    -}
+    -- Forecast
+    print =<< Forecast.fromCityName key Owm.Metric Owm.EN (CityName "Altendorf" "CH") DefaultCount
+    print =<< Forecast.fromZipCode key Owm.Metric Owm.DE (ZipCode 8001 "CH") (Count 1)
+
+    -- Forecast daily
+    print =<< ForecastDaily.fromZipCode key Owm.Metric Owm.DE (ZipCode 8001 "CH") (Count 1)
