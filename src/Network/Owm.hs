45fd737b1b3bdafbdd64f5c766019ebbc9e7c342
mayeranalytics <mmayer@mayeranalytics.com>
2018-11-10 15:48:52 +0100

    tidier interface

--- src/Network/Owm.hs
+++ src/Network/Owm.hs
@@ -2,26 +2,31 @@
 
 module Network.Owm
 (
+    CityName(..),
+    CityId(..),
+    Coords (..),
+    Count (..),
     Key (..),
-    CityName,
-    CityId,
-    Coords,
     Lat,
     Lon,
-    ZipCode,
-    CountryCode,
+    ZipCode(..),
+    CountryCode(..),
     Lang(..),
     Units(..),
     owmBaseUrl25,
     owmBaseUrl30,
-    makeParam, MakeParam
+    makeParam, MakeParam,
+    getOwm, getOwmWith
 ) where
 
 import           Prelude hiding (LT)
 import           Network.Wreq (Options)
-import           Network.Wreq (param)
-import           Control.Lens ((.~))
+import           Network.Wreq (param, defaults, getWith, responseBody)
+import           Control.Lens ((.~), (&), (^.))
 import qualified Data.Text as T
+import           Data.Maybe (catMaybes)
+import           Data.String (IsString(..))
+import           Data.Aeson(FromJSON, decode)
 
 owmBaseUrl25 :: String
 owmBaseUrl25 = "http://api.openweathermap.org/data/2.5/"
@@ -33,18 +38,58 @@ owmBaseUrl30 = "http://api.openweathermap.org/data/3.0/"
 class MakeParam a where
     makeParam ::  a -> Options -> Options
 
-type CityName = String
-type CityId = Int
-type Coords = (Lat, Lon)
 type Lat = Double
 type Lon = Double
-type ZipCode = Int
-type CountryCode = String
+
+data CountryCode = CountryCode String | AnyCountry deriving (Show)
+
+instance IsString CountryCode where
+    fromString s = if s == "" then AnyCountry else CountryCode s
+
 
 newtype Key = Key String deriving (Show)
 
 instance MakeParam Key where
     makeParam (Key key) = param "APPID" .~ [T.pack key]
+
+
+newtype CityId = CityId Int deriving (Show)
+
+instance MakeParam CityId where
+    makeParam (CityId i) = (param "id" .~ [T.pack $ show i])
+
+
+data CityName = CityName String CountryCode deriving (Show)
+
+instance MakeParam CityName where
+    makeParam (CityName n AnyCountry) = param "q" .~ [T.pack n]
+    makeParam (CityName n (CountryCode c)) = param "q" .~ [T.pack n']
+        where n' = n ++ "," ++  c
+
+
+data Coords = Coords Lat Lon deriving (Show)
+
+instance MakeParam Coords where
+    makeParam (Coords lat lon) = (param "lat" .~ [show' lat])
+                                . (param "lon" .~ [show' lon])
+                                where show' = T.pack . show
+   
+
+data Count = Count Int | DefaultCount deriving (Show)
+
+instance MakeParam Count where
+    makeParam DefaultCount = id
+    makeParam (Count i) = (param "cnt" .~ [T.pack $ show i])
+                                
+
+data ZipCode = ZipCode Int CountryCode deriving (Show)
+
+instance MakeParam ZipCode where
+    makeParam (ZipCode z AnyCountry) = param "zip" .~ [T.pack $ show z]
+    makeParam (ZipCode z (CountryCode c)) = param "q" .~ [T.pack z']
+        where z' = (show z) ++ "," ++  c
+    
+        
 
 -- |Owm lang setting. See https://openweathermap.org/current
 -- | awk 'BEGIN{FS=","}{print toupper($2)}' languages.txt | tr "\n" "|"
@@ -103,3 +148,17 @@ instance MakeParam Units where
     makeParam Standard = id
     makeParam Metric = param "units" .~ ["metric"]
     makeParam Imperial = param "units" .~ ["imperial"]
+
+getOwmWith :: FromJSON a => [Options->Options] -> Key -> Units -> Lang -> String -> IO (Maybe a)
+getOwmWith opts key units lang url = do
+    let params = [
+            makeParam key,
+            makeParam units,
+            makeParam lang
+            ] ++ opts
+        opts' = foldl (&) defaults params
+    r <- getWith opts' url >>= (return . (^. responseBody))
+    return $ decode r
+
+getOwm :: FromJSON a => Key -> Units -> Lang -> String -> IO (Maybe a)
+getOwm = getOwmWith []
